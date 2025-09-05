15d27b501a3258144ded77f76aac1d6c8a964f17
Yifu Tao <ethantao96@gmail.com>
2025-09-05 16:28:32 +0100

    chore: update ns commands

--- scripts/run_ns_batch.sh
+++ scripts/run_ns_batch.sh
@@ -4,19 +4,20 @@ for i in {1..8}; do
       --max-num-iterations 30000 \
       --steps-per-eval-image 10000 \
       --vis wandb \
+      --experiment-name sapling-0${i} \
       --pipeline.model.camera-optimizer.mode off \
-      nerfstudio-data --data /home/docker_dev/oxford_spires_dataset/data/saplings_nerf/2025-08-14-wytham/sapling-0${i}/raw/ \
+      nerfstudio-data --data /home/docker_dev/data/saplings_nerf/2025-08-14-wytham/sapling-0${i}/raw/ \
       --orientation_method none \
       --center_method none"
   echo -e "\033[1;36mRunning:\033[0m $train_cmd"
   eval $train_cmd
 
   # export point cloud
-  latest_run=$(ls -td /home/docker_dev/oxford_spires_dataset/outputs/unnamed/nerfacto/* | head -1)
+  latest_run=$(ls -td /home/docker_dev/saplings_nerf/outputs/sapling-0${i}/nerfacto/* | head -1)
   echo -e "\033[1;36mLatest run directory:\033[0m $latest_run"
   export_cmd="ns-export pointcloud \
       --load-config ${latest_run}/config.yml \
-      --output-dir /home/docker_dev/oxford_spires_dataset/ns_export \
+      --output-dir /home/docker_dev/saplings_nerf/eval_pointcloud \
       --num-points 5000000 \
       --remove-outliers True \
       --normal-method open3d \
